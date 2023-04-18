2f98080524bb6994b5c3d190ea2bbbdb86647fcf
玄冰 <1300272284@qq.com>
2023-04-18 20:10:13 +0800

    electron基础搭建
    主题：electron主进程
    变更：
    新增：基础版electron主进程

---
+++ electron/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: __dirname + "/preload.js",
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
