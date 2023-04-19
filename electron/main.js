fd0dc1d27d0ccec8a9391ef6dc686b288393daf0
玄冰 <1300272284@qq.com>
2023-04-19 22:48:45 +0800

    窗口定型
    窗口定型，主页样式开始编写

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,32 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+//热加载
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
+
 const createWindow = () => {
   const win = new BrowserWindow({
+    title: "玄冰是仓鼠",
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    maxHeight: 1080,
+    maxWidth: 1920,
+    //是否有阴影
+    hasShadow: false,
+    //是否置顶
+    alwaysOnTop: true,
+    //是否任务栏显示
+    skipTaskbar: true,
+    //是否无边框
+    // frame: false,
+    //自动隐藏菜单栏
+    autoHideMenuBar: true,
+
     webPreferences: {
-      preload: __dirname + "/preload.js",
       nodeIntegration: true,
     },
   });
