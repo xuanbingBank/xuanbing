5e799fde16e1971903e83efab104455170375aff
玄冰 <1300272284@qq.com>
2026-06-21 15:22:58 +0800

    取消跟踪dist

--- dist/electron/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * @file ���� preload �ŽӲ���¶���� API��
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exposeDesktopApi = void 0;
-const expose_api_1 = require("./ipcBus/preload/expose-api");
-Object.defineProperty(exports, "exposeDesktopApi", { enumerable: true, get: function () { return expose_api_1.exposeDesktopApi; } });
-(0, expose_api_1.exposeDesktopApi)();
--- dist/src/renderer/router/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * @file 路由元信息类型定义，扩展自原有 RouteMeta，新增 layout/menu/breadcrumb 等字段。
- */
-Object.defineProperty(exports, "__esModule", { value: true });
